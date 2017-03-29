801b7521efa13d09833c74c82984038a944d443c
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2017-03-29 15:03:32 -0700

    hellonode: Handle SIGINT, add hostname and \n to response (#15)
    * Handle SIGINT exit, add hostname and \n to resp
    
    - Ctrl+C on a Docker container sends SIGINT, which is not handled
      properly by default (see nodejs/node#4182). Added a signal handler.
    - Adding trailing newline to response for curl-friendliness.
    - Added log message indicating server is starting.
    - Added hostname to indicate that the container.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>
    
    * Address code review comments
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- hellonode/server.js
+++ hellonode/server.js
@@ -14,12 +14,23 @@
  * limitations under the License.
  */
 
-var http = require('http');
+const http = require('http');
+const os = require('os');
+
+const port = process.env.PORT || 8080;
+
+process.on('SIGINT', function() {
+  console.log('shutting down...');
+  process.exit(1);
+});
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log(`Received request for URL: ${request.url}`);
   response.writeHead(200);
-  response.end('Hello, World!');
+  response.end(`Hello, World!\nHostname: ${os.hostname()}\n`);
 };
+
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
