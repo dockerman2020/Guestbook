b2291a042a4c156c7b03b97dfd53b35a2ed62a2c
Jeffrey Rennie <rennie@google.com>
2019-08-08 15:52:07 -0700

    Small fixes as I worked through the sample. (#106)
    * Add dotnet.csproj project file.
    
    * lint
    
    * Rename dotnet.csproj to HelloGKE.csproj.

--- quickstart/dotnet/Program.cs
+++ quickstart/dotnet/Program.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace helloworld_csharp
+namespace HelloGKE
 {
     public class Program
     {
@@ -11,13 +25,25 @@ namespace helloworld_csharp
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            string url = String.Concat("http://0.0.0.0:", port);
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UsePortEnvironmentVariable();
+    }
 
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().UseUrls(url);
+    static class ProgramExtensions
+    {
+        // Google Cloud Run sets the PORT environment variable to tell this
+        // process which port to listen to.
+        public static IWebHostBuilder UsePortEnvironmentVariable(
+            this IWebHostBuilder builder)
+        {
+            string port = Environment.GetEnvironmentVariable("PORT");
+            if (!string.IsNullOrEmpty(port))
+            {
+                builder.UseUrls($"http://0.0.0.0:{port}");
+            }
+            return builder;
         }
     }
 }
